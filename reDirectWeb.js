74a73708b413f400784763142260f98e990a79bb
jclhr <425044131@qq.com>
2022-10-30 11:39:38 +0800

    2

--- reDirectWeb.js
+++ reDirectWeb.js
@@ -5,7 +5,7 @@
 // @description  nihaoa
 // @description:zh-cn hai
 // @author       jclhr
-// @match        https://www.msnzy1.xyz/cn/home/web/*
+// @match        https://www.msnzy1.xyz/cn/home/web/* https://osc.msnzy1.xyz/*
 // @icon         https://greasyfork.org/vite/assets/blacklogo96.e0c2c761.png
 // @grant        none
 // @license    GPL-3.0-only
