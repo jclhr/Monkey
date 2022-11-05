4064028b4f7dce2b30fba553ea8330208e0fd72c
jclhr <425044131@qq.com>
2022-11-05 10:36:42 +0800

    a

--- reDirectWeb.js
+++ reDirectWeb.js
@@ -7,11 +7,20 @@
 // @author       jclhr
 // @match        https://www.msnzy1.xyz/cn/home/web/*
 // @match        https://osc.msnzy1.xyz/*
+// @match        *://hsex.men/*
 // @icon         https://greasyfork.org/vite/assets/blacklogo96.e0c2c761.png
 // @grant        none
 // @license    GPL-3.0-only
 // ==/UserScript==
- 
+
 (() => {
+    //msn
     document.getElementById("dgf_pc").remove();
+    //men
+    document.getElementsByClassName("col-xs-12 col-md-6").remove();
+    document.getElementsByClassName("col-xs-12 col-md-6").remove();
+    document.getElementById("player-container").remove();
+
+    //document.getElementsByTagName
+
 })();
