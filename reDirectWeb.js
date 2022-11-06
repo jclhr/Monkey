56a1a9c36ac356c963421e78053177d0f11a7545
jclhr <425044131@qq.com>
2022-11-06 21:57:35 +0800

    xiufu

--- reDirectWeb.js
+++ reDirectWeb.js
@@ -2,17 +2,29 @@
 // @name         newScript
 // @namespace    http://name.fun/
 // @version      0.3
-// @description  88kanqiu remove ads
+// @description  nihaoa
 // @description:zh-cn hai
 // @author       jclhr
-// @match        https://http://www.88kanqiu.tv/*
+// @match        https://www.msnzy1.xyz/cn/home/web/*
+// @match        https://osc.msnzy1.xyz/*
+// @match        https://byy.msnzy1.xyz/*
+// @match        https://hsex.men/*
+// @match        *://hsex.men/*
 // @icon         https://greasyfork.org/vite/assets/blacklogo96.e0c2c761.png
 // @grant        none
-// @license      GPL-3.0-only
+// @license    GPL-3.0-only
 // ==/UserScript==
 
 (() => {
     "use strict";
-    document.getElementById("sidebar-wechat").remove();
-    document.querySelector('.sss-container').remove();
+    //msn
+    document.getElementById("dgf_pc").remove();
+    //men
+    document.getElementById("container").remove();
+    var topAds=document.getElementsByClassName("col-xs-12 col-md-6")[0];
+    topAds.remove()
+    //document.getElementById("player-container").remove();
+
+    //document.getElementsByTagName
+
 })();
