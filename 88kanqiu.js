8d39934b26a7c850e6c0b00d37d1ef54d726b75e
jclhr <425044131@qq.com>
2022-11-06 22:00:02 +0800

    xiugu1

--- 88kanqiu.js
+++ 88kanqiu.js
@@ -5,7 +5,7 @@
 // @description  88kanqiu remove ads
 // @description:zh-cn hai
 // @author       jclhr
-// @match        https://http://www.88kanqiu.tv/*
+// @match        http://www.88kanqiu.tv/*
 // @icon         https://greasyfork.org/vite/assets/blacklogo96.e0c2c761.png
 // @grant        none
 // @license      GPL-3.0-only
